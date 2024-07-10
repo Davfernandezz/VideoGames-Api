113e007d977f1ef5a87855584aee2f1c391bafd6
David Fernadez <daffevafernandez@gmail.com>
2024-07-10 12:44:37 +0200

    feat: MONGO_URI connection

--- src/server.js
+++ src/server.js
@@ -1,21 +1,30 @@
 // const express = require('express')
 import express from 'express';
 import 'dotenv/config';
+import { dbConnection } from './database/db.js';
 
 
 const app = express()
 
 const PORT = process.env.PORT || 4000
 
-app.get('/healthy', (req, res)=>{
+app.get('/healthy', (req, res) => {
     res.json(
         {
-            success:true,
+            success: true,
             message: "Server is healthy"
         }
     )
 })
 
-app.listen(PORT, () => {
-    console.log(`Server running ${PORT}`);
-})
+
+dbConnection()
+    .then(() => {
+        console.log('Database Connected');
+        app.listen(PORT, () => {
+            console.log(`Server running ${PORT}`);
+        });
+    })
+    .catch(error => {
+        console.log('Error conecction database: ' + error.message);
+    })
