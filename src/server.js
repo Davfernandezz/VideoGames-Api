41c8ba291812b3c9cbbb50bc38ac87e8da078392
David Fernadez <daffevafernandez@gmail.com>
2024-07-10 10:32:00 +0200

    feat: dotenv added to proyect

--- src/server.js
+++ src/server.js
@@ -1,6 +1,12 @@
-const express = require('express')
+// const express = require('express')
+import express from 'express';
+import 'dotenv/config';
+
+
 const app = express()
+
 const PORT = process.env.PORT || 4000
+
 app.listen(PORT, () => {
     console.log(`Server running ${PORT}`);
 })
