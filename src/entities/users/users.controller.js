21739793eed0af5b2ecad85b29883f597f9eee4a
David Fernadez <daffevafernandez@gmail.com>
2024-07-12 09:43:27 +0200

    feat: users controllers login

--- src/entities/users/users.controller.js
+++ src/entities/users/users.controller.js
@@ -1,6 +1,8 @@
 import bcrypt from "bcrypt";
 import User from "./user.models.js";
+import jwt from "jsonwebtoken"
 
+//POST
 export const register = async (req, res) => {
     try {
         const { email, password } = req.body
@@ -11,17 +13,61 @@ export const register = async (req, res) => {
             email: email,
             password: hashedPassword
         })
-
         res.status(201).json({
             success: true,
             message: "User created",
             data: newUser
         })
-
     } catch (error) {
         res.status(500).json({
             success: false,
             message: "Error registering user",
+            error: error.message
+        })
+    }
+}
+
+//POST
+export const login = async (req, res) => {
+    try {
+        const { email, password } = req.body
+
+        if (!email || !password) {
+            return res.status(400).json({
+                success: false,
+                message: "Email and Password are needed"
+            })
+        }
+        const user = await User.findOne({
+            email: email
+        })
+        if (!user) {
+            return res.status(400).json({
+                success: false,
+                message: "Email or password not valid"
+            })
+        }
+        if (!bcrypt.compareSync(password, user.password)) {
+            return res.status(400).json({
+                succes: false,
+                message: "Email or password not valid"
+            })
+        }
+       const token = jwt.sign({
+            id: user.id,
+            email:user.email,
+            role: user.role
+          }, process.env.JWT_SECRET, { expiresIn: '2h' });
+
+        return res.status(200).json({
+            success: true,
+            Message: "User logged",
+            token: token
+        })
+    } catch (error) {
+        res.status(500).json({
+            success: false,
+            message: "Error log in user",
             error: error.message
         })
     }
